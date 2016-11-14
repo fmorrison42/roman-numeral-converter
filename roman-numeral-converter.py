32ba7c8801416888fec6e99fc22a8a7375d5aebc
Frank Morrison <Frank Morrison>
2016-11-13 19:41:23 -0700

    initial commit

---
+++ roman-numeral-converter.py
@@ -0,0 +1,16 @@
+"""
+roman numeral converter to arabic numerals
+"""
+
+unum = 1
+vi = 5
+decem = 10
+lira = 50
+cent = 100
+dixcent = 500
+mille = 1000
+
+romanInput = int(input("What is your roman number in question?"))
+
+romanInput.split()
+
